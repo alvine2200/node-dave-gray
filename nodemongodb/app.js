20a86bb1493510ee81f62bf2fa2ec9619b0f83c4
alvine2200 <alvinellavu@gmail.com>
2023-01-16 12:28:23 +0300

    mongo connected

--- nodemongodb/app.js
+++ nodemongodb/app.js
@@ -1,7 +1,24 @@
 const express = require("express");
-const mongo = require("mongoose");
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 8000;
+const dotenv = require("dotenv");
+const url = process.env.MONGO_URL;
 const app = express();
+
+dotenv.config();
+
+async function connect() {
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(
+      "mongodb+srv://codestepbystep:codestepbystep@cluster0.hp6rxd3.mongodb.net/?retryWrites=true&w=majority"
+    );
+    console.log("Connected to Mongo db south Africa");
+  } catch (error) {
+    console.error(error);
+  }
+}
+connect();
 
 app.get("/", (req, res) => {
   res.send("Hello Word");
